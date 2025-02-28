8abcddcc0744b00be1972aab1a0d501d09953b23
Leandro Candido <leandrocandido638@gmail.com>
2025-02-28 16:54:34 -0400

    up

---
+++ Exercicio_Entity/exercicio/Software.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercicio
+{
+    [Table("software")]
+    public class Software
+    {
+        [Key]
+        [Column("id_software")]
+        public int Id_soft { get; set; }
+
+        [Column("produto")]
+        public string Produto { get; set; } = string.Empty;
+
+        [Column("hardDisk")]
+        public int HardDisk { get; set; }
+
+        [Column("memora_ram")]
+        public int MemoriaRam { get; set; }
+
+        // Chave estrangeira para a Maquina
+        [Column("fk_maquina")]
+        public int FkMaquina { get; set; }
+
+        // Definindo o relacionamento com a Maquina
+        [ForeignKey("fk_maquina")]
+        public Maquina Maquina { get; set; }
+    }
+}
